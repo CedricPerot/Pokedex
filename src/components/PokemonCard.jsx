fd8936f9ea9f9bdb500f9ef327aa5681faf21275
CedricPerot <cedric.perot@gmail.com>
2023-03-28 12:04:58 +0200

    creation tableau pokemon et condition si pas image

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,19 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+const pokemon = pokemonList[1].name;
+const pokemonPicture = pokemonList[1].imgSrc;
+
 function PokemonCard() {
-    return (<figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbosaur"/><figcaption>Bulbosaur</figcaption>
-    </figure>);
+    return pokemonPicture ? (<figure><img src={pokemonPicture} alt="Bulbosaur"/><figcaption>{pokemon}</figcaption>
+    </figure>) : <p>???</p>;
   }
   
   export default PokemonCard;
