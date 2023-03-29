b2e2057fd651249186c443bfb3fa9ed6e19fb5f1
CedricPerot <cedric.perot@gmail.com>
2023-03-29 10:35:09 +0200

    ajout des prop types

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,16 @@
 // const pokemon = pokemonList[1].name;
 // const pokemonPicture = pokemonList[1].imgSrc;
 
-function PokemonCard(props) {
-    return props.imgSrc ? (<figure><img src={props.imgSrc} alt="Bulbosaur"/><figcaption>{props.name}</figcaption>
-    </figure>) : <p>???<figcaption>{props.name}</figcaption></p>;
+function PokemonCard(pokemon) {
+    return pokemon.imgSrc ? (<figure><img src={pokemon.imgSrc} alt="Bulbosaur"/><figcaption>{pokemon.name}</figcaption>
+    </figure>) : <p>???<figcaption>{pokemon.name}</figcaption></p>;
+  }
+
+  PokemonCard.PropTypes = {
+    pokemonList : PropTypes.arrayOf(PropTypes.shape({
+      name : PropTypes.string.isRequired,
+      imgSrc : PropTypes.string,
+    }).isRequired).isRequired,
   }
   
   export default PokemonCard;
