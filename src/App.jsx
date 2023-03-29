569d91d736e94f09f615490a800011de215876a2
CedricPerot <cedric.perot@gmail.com>
2023-03-29 14:47:07 +0200

    ajout limites tableau pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,57 @@ import './App.css'
 import PokemonCard from './components/PokemonCard'
 const pokemon = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  }
-];
+      id : "1",
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      id : "2",
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      id : "3",
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      id : "4",
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      id : "5",
+      name: "mew",
+    },
+  ];
 
 function App() {
-  // const [count, setCount] = useState(0)
+  
+  const [count, setCount] = useState(0);
 
+  const handleNext = () => {
+    setCount(count + 1);
+  }
+  
+  const handlePrevious = () => {
+    setCount(count - 1);
+  }
   return (
-  <div className="App">
-    <PokemonCard name = {pokemon[0].name} imgSrc = {pokemon[0].imgSrc}/>
-  </div>);
+    <div className="App">
+      <PokemonCard name = {pokemon[count].name} imgSrc = {pokemon[count].imgSrc}/>
+      <div>
+            {count > 0 ? <button onClick = {handlePrevious} className="button">Previous</button> : <button className="button">Stop</button>}
+            {count < pokemon.length - 1 ? <button onClick = {handleNext} className="button">Next</button> : <button className="button">Stop</button>}
+      </div>
+    </div>
+    );
+  
 }
+
 
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,15 +1,14 @@
-// const pokemon = pokemonList[1].name;
-// const pokemonPicture = pokemonList[1].imgSrc;
 import PropTypes from "prop-types";
 
 function PokemonCard(pokemon) {
-    return pokemon.imgSrc ? (<figure><img src={pokemon.imgSrc} alt="Bulbosaur"/><figcaption>{pokemon.name}</figcaption>
+    return pokemon.imgSrc ? (<figure><img src={pokemon.imgSrc} alt={pokemon.name}/><figcaption>{pokemon.name}</figcaption>
     </figure>) : <p>???<figcaption>{pokemon.name}</figcaption></p>;
   }
 //ajout des prop types
 
   PokemonCard.PropTypes = {
-    pokemonList : PropTypes.arrayOf(PropTypes.shape({
+    pokemon : PropTypes.arrayOf(PropTypes.shape({
+      id : PropTypes.string.isRequired,
       name : PropTypes.string.isRequired,
       imgSrc : PropTypes.string,
     }).isRequired).isRequired,
