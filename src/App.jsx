8fabc7b1a90a9ddd7637d42e2dbc13eb38c09613
CedricPerot <cedric.perot@gmail.com>
2023-04-05 11:14:19 +0200

    passage des donnees dans un fichier separe

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,44 @@
 import './App.css'
 import PokemonCard from "./components/PokemonCard"
+import Pokemons from "./components/Pokemons"
 import { useState } from 'react';
 import NavBar from './components/NavBar';
 
-const pokemonList = [
-  {
-      name: "Bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "Charmander",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-    },
-    {
-      name: "Squirtle",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-    },
-    {
-      name: "Pikachu",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    },
-    {
-      name: "Mew",
-    },
-  ]; 
+// const pokemonList = [
+//   {
+//       name: "Bulbasaur",
+//       imgSrc:
+//         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+//     },
+//     {
+//       name: "Charmander",
+//       imgSrc:
+//         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+//     },
+//     {
+//       name: "Squirtle",
+//       imgSrc:
+//         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+//     },
+//     {
+//       name: "Pikachu",
+//       imgSrc:
+//         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+//     },
+//     {
+//       name: "Mew",
+//       imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/30.png"
+//     },
+//   ]; 
 
 function App() {
   const [pokemonIndex, setPokemonIndex] = useState(0);
 
   return (<div>
     <div className="pokeball">
-      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+      <PokemonCard pokemon={Pokemons[pokemonIndex]}/>
     </div>
-    <div>{pokemonList.map((pokemonIndex, index) =>
+    <div>{Pokemons.map((pokemonIndex, index) =>
       <NavBar setPokemonIndex={setPokemonIndex} pokemonIndex={pokemonIndex} index={index}/>)}
     </div>
     </div>
---
+++ src/components/Pokemons.jsx
@@ -0,0 +1,28 @@
+const Pokemons = [
+    {
+        name: "Bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "Charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "Squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "Pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "Mew",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/30.png"
+      },
+    ]; 
+
+    export default Pokemons;
